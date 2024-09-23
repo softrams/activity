39b21aed2c5e4bc2fda59d9b8328ba62a42ab7b2
Austen Stone <austenstone@github.com>
2024-09-23 10:11:19 -0400

    chore: Update code to get members of organization
    This commit updates the code in both the `index.js` and `index.ts` files to get the members of the specified organization. It adds a log message to indicate that the code is retrieving the members of the organization. This change improves the functionality of the code.

--- dist/index.js
+++ dist/index.js
@@ -29149,13 +29149,12 @@ const run = async () => {
     const users = await octokit.paginate(octokit.rest.orgs.listMembers, {
         org: input.organization,
     });
-    (0, core_1.info)(JSON.stringify(users, null, 2));
     const since = new Date();
     since.setDate(since.getDate() - 90);
     const formattedSince = since.toISOString().slice(0, 10);
     for (const user of users) {
         const commits = await octokit.rest.search.commits({
-            q: `author:${user} org:${input.organization} committer-date:<${formattedSince}`,
+            q: `author:${user.login} org:${input.organization} committer-date:<${formattedSince}`,
         });
         (0, core_1.info)(JSON.stringify(commits, null, 2));
     }
--- src/index.ts
+++ src/index.ts
@@ -24,17 +24,14 @@ export const run = async (): Promise<void> => {
   const users = await octokit.paginate(octokit.rest.orgs.listMembers, {
     org: input.organization,
   });
-
-  info(JSON.stringify(users, null, 2));
-
-  // date object 90 days ago
+  
   const since = new Date();
   since.setDate(since.getDate() - 90);
   const formattedSince = since.toISOString().slice(0, 10);
 
   for (const user of users) {
     const commits = await octokit.rest.search.commits({
-      q: `author:${user} org:${input.organization} committer-date:<${formattedSince}`,
+      q: `author:${user.login} org:${input.organization} committer-date:<${formattedSince}`,
     });
     info(JSON.stringify(commits, null, 2));
   }
